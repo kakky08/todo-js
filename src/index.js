55b3527e0a6510138c9dfa5281b2c3861ddfe8d2
kakky08 <77216691+kakky08@users.noreply.github.com>
2021-02-15 12:46:42 +0900

    追加機能の実装

--- src/index.js
+++ src/index.js
@@ -13,8 +13,24 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const dleteButton = document.createElement("button");
+  dleteButton.innerText = "削除";
+  dleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(dleteButton);
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
